05e38792ea1e38974c277f68ed98973ac67476cf
r4ideny <juanmillfck@gmail.com>
2025-06-25 14:42:44 -0300

    adaptación a nuevo NuGet

--- DoneIt/obj/Debug/net9.0/DoneIt.AssemblyInfo.cs
+++ DoneIt/obj/Debug/net9.0/DoneIt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoneIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b059ede6ae03f9f6b0112f39af146dddc0e8beec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264e65da75cf05e746bc0d104ddf2b9ba567b8a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoneIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoneIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
